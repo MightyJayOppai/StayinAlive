9a7c2bad7d748840e45bcd41421f60e21b496eab
Khatim <kahtim.dar@gmail.com>
2019-08-29 02:08:56 +0400

    Added unity chan. Working on even more camera placements with Ajay and Friday

---
+++ Stayin' Alive/Assets/Individual Sections/Khatim/Scripts/VCamManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VCamManager : MonoBehaviour
+{
+    [SerializeField] private GameObject vCamA1;
+    [SerializeField] private GameObject vCamA2;
+    [SerializeField] private GameObject vCamB1;
+    [SerializeField] private GameObject vCamB3;
+    [SerializeField] private GameObject vCamC1;
+    [SerializeField] private GameObject vCamC2Pan;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "AreaA1")
+        {
+            vCamA1.SetActive(true);
+            vCamA2.SetActive(false);
+            Debug.Log("Cam Switched to VCamA1");
+        }
+
+        if (other.tag == "AreaA2")
+        {
+            vCamA2.SetActive(true);
+            vCamA1.SetActive(false);
+            vCamB1.SetActive(false);
+            vCamC1.SetActive(false);
+            Debug.Log("Cam Switched to VCamA2");
+        }
+
+        if (other.tag == "AreaB1")
+        {
+            vCamB1.SetActive(true);
+            vCamA2.SetActive(false);
+            vCamB3.SetActive(false);
+            Debug.Log("Cam Switched to VCamB1");
+        }
+
+        if (other.tag == "AreaB2")
+        {
+            vCamB3.SetActive(true);
+            vCamB1.SetActive(false);
+            Debug.Log("Cam Switched to VCamB3");
+        }
+
+        if (other.tag == "AreaC1")
+        {
+            vCamC1.SetActive(true);
+            vCamA2.SetActive(false);
+            Debug.Log("Cam Switched to VCamC1");
+        }
+
+        if (other.tag == "AreaC2")
+        {
+            vCamC2Pan.SetActive(true);
+            vCamC1.SetActive(false);
+            Debug.Log("Cam Switched to VCamC2");
+        }
+    }
+}
