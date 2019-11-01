af94b5bc9ff96d6f7185e4361d64e4ec926ef91e
Ajay Rawat <a.rawat@saedubai.com>
2019-11-01 19:11:09 +0400

    Added new floor texture and moved Textures and Materials folder to Assets From Animators

---
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/CaseFileTransition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseFileTransition : MonoBehaviour
+{
+    public GameObject sceneTransitionScreen;
+    public Collider col;    
+    public GameObject transitionText;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
