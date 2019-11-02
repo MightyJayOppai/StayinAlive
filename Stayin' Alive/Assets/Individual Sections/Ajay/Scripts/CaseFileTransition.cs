fa5da4bd36b089bc013e3f806d50b54b83f497bb
Ajay Rawat <a.rawat@saedubai.com>
2019-11-02 17:04:54 +0400

    Added the Map Transtion to the Folder in the office (Very Simple - To be edited)

--- Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/CaseFileTransition.cs
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/CaseFileTransition.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CaseFileTransition : MonoBehaviour
 {
@@ -9,12 +10,43 @@ public class CaseFileTransition : MonoBehaviour
     public GameObject transitionText;
     void Start()
     {
-        
+        sceneTransitionScreen.SetActive(false);
+        transitionText.SetActive(false);
     }
 
     
     void Update()
     {
-        
+        if(col!= null && Input.GetKeyDown(KeyCode.E))
+        {
+            sceneTransitionScreen.SetActive(!sceneTransitionScreen.activeSelf);
+            if (sceneTransitionScreen.activeSelf)
+            {
+                //GetComponent<MapSceneManager>().TransitionToAlleyway();
+                UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+                Debug.Log("Go to the Alleyway");
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //To display the Screen once the player enters the collider
+        if (other.tag == "Player")
+        {
+            col = other;
+            sceneTransitionScreen.SetActive(true);
+            transitionText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //To remove the Screen once the player moves away from the collider
+        if (other.tag == "Player")
+        {
+            col = null;
+            sceneTransitionScreen.SetActive(false);
+            transitionText.SetActive(false);
+        }
     }
 }
