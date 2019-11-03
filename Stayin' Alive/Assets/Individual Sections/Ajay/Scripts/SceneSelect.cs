fd1c1dc9cd4d2603bc36e74dbbf47887fba42ec4
Ajay Rawat <a.rawat@saedubai.com>
2019-11-03 04:16:03 +0400

    Added a Pause Menu and the Main Menu (Placeholders)

--- Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/SceneSelect.cs
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/SceneSelect.cs
@@ -15,10 +15,24 @@ public class SceneSelect : MonoBehaviour
     }
     public void MoveToAlleyway()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(4);
     }
     public void MoveToBasement()
     {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+    public void LoadMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        //Debug.Log("Loading Menu");
+    }
+    public void MoveToOffice()
+    {
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        //Debug.Log("Im'ma Head out");
     }
 }
