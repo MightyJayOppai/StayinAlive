188ab3c58711ddd177f4ed762d5276cdcee0c4f9
Ajay Rawat <a.rawat@saedubai.com>
2019-11-12 21:31:04 +0400

    Tested out new textures for the alleyway buildings and added the two remaining clues that the player will be able to pickup to the log (placeholders)

---
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/Clue3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue3 : ClueItemBase
+{
+    public override string Name
+    {
+        get
+        {
+            return "Clue3";
+        }
+    }
+    public override void OnUse()
+    {
+        base.OnUse();
+    }
+}
---
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/Clue4.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clue4 : ClueItemBase
+{
+    public override string Name
+    {
+        get
+        {
+            return "Clue4";
+        }
+    }
+    public override void OnUse()
+    {
+        base.OnUse();
+    }
+}
