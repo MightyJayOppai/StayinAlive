34ee014d7f9024cf7387e7d4323e9316dc943d08
Ajay Rawat <a.rawat@saedubai.com>
2019-11-29 14:10:52 +0400

    Adjusted the lighting in the alleyway and re-added the Clue Rotation Script

---
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/ClueRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClueRotation : MonoBehaviour
+{
+    public Camera obCam;
+    Vector3 mPrevPos = Vector3.zero;
+    Vector3 mPosDelta = Vector3.zero;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mPosDelta = Input.mousePosition - mPrevPos;
+
+            if(Vector3.Dot(transform.up, Vector3.up) >= 0)
+            {
+                transform.Rotate(transform.up, -Vector3.Dot(mPosDelta, obCam.transform.right), Space.World);
+            }
+            else
+            {
+                transform.Rotate(transform.up, Vector3.Dot(mPosDelta, obCam.transform.right), Space.World);
+            }
+
+            transform.Rotate(obCam.transform.right, Vector3.Dot(mPosDelta, obCam.transform.up), Space.World);
+        }
+        
+        mPrevPos = Input.mousePosition;
+    }
+}
