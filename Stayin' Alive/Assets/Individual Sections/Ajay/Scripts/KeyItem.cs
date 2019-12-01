f35db3fa92557b7a6b4bcfc67236f223e3fd75cd
Ajay Rawat <a.rawat@saedubai.com>
2019-12-01 17:01:40 +0400

    Changed the texture of the Warehouse in the Docks Added the Object Interaction and Key Item Script Added a new cam for the docking building in the Alleyway Added the new key model

---
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/KeyItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    public static int keyItemCount;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            keyItemCount += 1;
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ Stayin' Alive/Assets/Individual Sections/Ajay/Scripts/ObjectInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    public GameObject oldProp;
+    public GameObject newProp;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && KeyItem.keyItemCount>0)
+        {
+            KeyItem.keyItemCount--;
+            oldProp.SetActive(false);
+            newProp.SetActive(true);
+        }
+    }
+}
